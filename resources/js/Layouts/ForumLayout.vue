78385bc4b9695c5193e9ea77009e07a3c70256c9
Alex Garrett-Smith <alex@codecourse.com>
2023-05-11 11:09:32 +0100

    Episode 04

---
+++ resources/js/Layouts/ForumLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <AuthenticatedLayout>
+        <div class="py-12 max-w-7xl mx-auto space-y-6 md:space-y-0 md:grid grid-cols-7 gap-6 sm:px-6 lg:px-8">
+            <div class="col-span-2">
+                <slot name="side" />
+            </div>
+            <div class="col-span-5">
+                <slot />
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<script setup>
+    import AuthenticatedLayout from './AuthenticatedLayout.vue'
+</script>
--- resources/js/Pages/Forum/Index.vue
+++ resources/js/Pages/Forum/Index.vue
@@ -1,18 +1,22 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import ForumLayout from '@/Layouts/ForumLayout.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
     <Head title="Forum" />
 
-    <AuthenticatedLayout>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900"></div>
+    <ForumLayout>
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900"></div>
+        </div>
+
+        <template #side>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    Side
                 </div>
             </div>
-        </div>
-    </AuthenticatedLayout>
+        </template>
+    </ForumLayout>
 </template>
